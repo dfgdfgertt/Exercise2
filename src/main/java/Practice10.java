7d83e87f00e154ed5236528f2ee38b07dcdfb670
dfgdfgertt <dfgdfgertt@github.com>
2021-08-07 10:44:40 +0700

    + add Practice10

---
+++ src/main/java/Practice10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practice10 {
+
+    public static void main(String[] ag){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nhập năm: ");
+        int year = input.nextInt();
+        System.out.println("Năm này là năm nhuận: " + yearCheck(year));
+    }
+
+    public static boolean yearCheck(int year){
+        if ((year%4 == 0) && (year%100 != 0))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
